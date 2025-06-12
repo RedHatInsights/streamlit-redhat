7d1e8fffeae6ead7b0d0a117cba9b1332548de1d
Juan Diaz Suarez <jdiazsua@redhat.com>
2025-06-12 13:31:33 +0200

    refactor: update sample app

--- streamlit_redhat/app.py
+++ streamlit_redhat/app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 from streamlit_redhat.sso import user_info, login
 from streamlit_redhat.monitoring import streamlit_registry
+import streamlit_redhat.style as css
+
+css.add_logo()
+
+st.title("Welcome to Red Hat's Streamlit demo")
+
+css.load_template("red-hat.css")
+
+with st.sidebar:
+    st.write("This is some text")
+    st.button("A button")
 
 from prometheus_client import Counter
 
@@ -22,7 +33,7 @@ USERS_COUNT = get_users_count()
 
 
 if user_info.get("is_logged_in"):
-    st.title(f"Hello, {user_info['name']}!")
+    st.write(f"Hello, {user_info['name']}!")
     st.write(f"Your email is {user_info['email']}")
     USERS_COUNT.labels(name=user_info["name"]).inc()
 else:
